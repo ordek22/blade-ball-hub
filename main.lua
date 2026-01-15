44c14b1235559b79d72ddd604e273e95352162ec
ordek22 <ozanst520@gmail.com>
2026-01-15 17:18:33 +0300

    main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ordek22/blade-ball-hub/main/main.lua"))()
+pcall(function()
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Furk Ultra",
+        Text = "Script Yukleniyor...",
+        Duration = 3
+    })
+end)
+
+task.wait(1)
+
+local ok, err = pcall(function()
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/ordek22/blade-ball-hub/main/hub.lua"
+    ))()
+end)
+
+if not ok then
+    warn(err)
+end
